337fcd5570c08849a8ec6d6ee19210c84a06343a
Wess Cope <wcope@me.com>
2012-05-22 20:57:34 -0400

    Initial Commit

---
+++ WCXMLParserExample/WCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WCAppDelegate.h
+//  WCXMLParserExample
+//
+//  Created by Wess Cope on 5/22/12.
+//  Copyright (c) 2012 American Business Journals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WCXMLParserExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WCXMLParserExample
+//
+//  Created by Wess Cope on 5/22/12.
+//  Copyright (c) 2012 American Business Journals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WCAppDelegate class]));
+    }
+}
